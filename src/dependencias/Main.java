ab7b489a0176ab692e649719665e410ca89ea70f
Paulo Ricardo <pauloricardossfilho@gmail.com>
2023-01-31 14:07:08 -0300

    Finalizado

--- src/dependencias/Main.java
+++ src/dependencias/Main.java
@@ -12,9 +12,9 @@ public class Main {
 		System.out.println("How many employees will be registered? ");
 		int n = sc.nextInt();
 		for(int c = 0; c < n; c++) {
-			System.out.println("Employee #"+c+":");
+			System.out.println("Employee #"+(c+1)+":");
 			System.out.print("Id: ");
-			int id = sc.nextInt();
+			Integer id = sc.nextInt();
 			while (hasId(emp, id)) {
 				System.out.print("Id already taken. Try again: ");
 				id = sc.nextInt();
@@ -23,21 +23,22 @@ public class Main {
 			sc.nextLine();
 			String name = sc.nextLine();
 			System.out.print("Salary: ");
-			double salary = sc.nextDouble();
+			Double salary = sc.nextDouble();
 			emp.add(new Employee(id, name, salary));
 		}
 		
 		System.out.println("Enter the employee id that will have salary increase : ");
 		Integer id = sc.nextInt();
 		
-		Employee empf = emp.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		if (empf == null) {
+		
+		Integer pos = findId(emp, id);
+		if (pos == null) {
 			System.out.println("This id does not exist!");
 		}
 		else {
 			System.out.print("Enter the percentage: ");
 			double percentage = sc.nextDouble();
-			empf.increaseSalary(percentage);
+			emp.get(pos).increaseSalary(percentage);
 		}
 		
 		System.out.println("List of employees: ");
@@ -50,6 +51,16 @@ public class Main {
 	}
 	public static boolean hasId(List<Employee> emp, int id) {
 		Employee emp2 = emp.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		return emp2 != null;
+		return emp2 != null;			
 	}
+	
+	public static Integer findId(List<Employee> emp, int id) {
+		for(int c = 0; c < emp.size(); c++) {
+			if(emp.get(c).getId() == id) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
 }
